b9b8592441d1c09e6764544729830cf602e19e19
JPCabral04 <jpqcabral04@gmail.com>
2025-01-19 20:56:09 -0300

    ✨ Implement basic arithmetic functions and add event listeners for user input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Testando");
+const input1 = document.getElementById('num1') as HTMLInputElement;
+const input2 = document.getElementById('num2') as HTMLInputElement;
+const buttonSoma = document.getElementById('buttonSomar') as HTMLInputElement;
+const buttonSubtrair = document.getElementById('buttonSubtrair') as HTMLInputElement;
+
+function somar(a : number,b: number) {
+    return a+b;
+}
+
+function subtrair(a : number,b: number) {
+    return a-b;
+}
+
+buttonSoma.addEventListener('click', () => {
+    const resultado = somar(Number(input1.value), Number(input2.value));
+    console.log(resultado);
+})
+
+buttonSubtrair.addEventListener('click', () => {
+    const resultado = subtrair(Number(input1.value), Number(input2.value));
+    console.log(resultado);
+})
