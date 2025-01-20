70b4d65d055ccc8d14d81e47947503616f604f80
JPCabral04 <jpqcabral04@gmail.com>
2025-01-19 21:14:31 -0300

    ✨ Refactor arithmetic operations into a single function with type-safe parameters

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,36 @@ const input2 = document.getElementById('num2') as HTMLInputElement;
 const buttonSoma = document.getElementById('buttonSomar') as HTMLInputElement;
 const buttonSubtrair = document.getElementById('buttonSubtrair') as HTMLInputElement;
 
-function somar(a : number,b: number) {
-    return a+b;
+type Operacoes = "SOMAR" | "SUBTRAIR"
+
+interface Valores{
+    tipo : Operacoes;
+    a : number;
+    b : number;
 }
 
-function subtrair(a : number,b: number) {
-    return a-b;
+function operacao({tipo,a,b}: Valores) {
+    if(tipo === "SOMAR") {
+        return a+b;
+    } else {
+        return a-b;
+    }
 }
 
 buttonSoma.addEventListener('click', () => {
-    const resultado = somar(Number(input1.value), Number(input2.value));
+    const resultado = operacao({
+        tipo : "SOMAR",
+        a: Number(input1.value),
+        b: Number(input2.value),
+    });
     console.log(resultado);
 })
 
 buttonSubtrair.addEventListener('click', () => {
-    const resultado = subtrair(Number(input1.value), Number(input2.value));
+    const resultado = operacao({
+        tipo : "SUBTRAIR",
+        a: Number(input1.value),
+        b: Number(input2.value),
+    });
     console.log(resultado);
 })
